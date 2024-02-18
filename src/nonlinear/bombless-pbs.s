6734e06ba374e155d315489eb3e2b8f1472c0c79
Callie LeFave <antymew@gmail.com>
2024-02-17 23:03:57 -0500

    Fix bombless pbs patch

--- src/nonlinear/bombless-pbs.s
+++ src/nonlinear/bombless-pbs.s
@@ -3,12 +3,13 @@
 .org 08005EBEh
 .area 1Ah
 	ldrb	r1, [r0, SamusUpgrades_ExplosiveUpgrades]
-	lsr		r0, r1, ExplosiveUpgrade_Bombs + 1
-	bcs		08005EE0h
-	lsr		r0, r1, ExplosiveUpgrade_PowerBombs + 1
-	bcc		08005EE0h
 	ldrb	r0, [r2, SamusState_SecondaryWeaponSelect]
 	lsr		r0, #5
-	bcc		08005EE0h
-	b		08005ED4h
+	bcc		@@check_bombs
+	lsr		r0, r1, ExplosiveUpgrade_PowerBombs + 1
+	bcs		08005ED4h
+@@check_bombs:
+	lsr		r0, r1, ExplosiveUpgrade_Bombs + 1
+	bcs		08005EE0h
+	b		08005EE4h
 .endarea
