3ae2fb6ef62c455192aef08596cacc2dccf702d2
Callie LeFave <antymew@gmail.com>
2024-02-27 14:45:56 -0500

    Replace demo implementation for more flexibility

--- src/nonlinear/demos.s
+++ src/nonlinear/demos.s
@@ -1,67 +1,181 @@
 ; Reworks demos to work properly with non-linear patches.
 
-.org 0807DF04h
-.area 60h
-	push	{ r4-r6, lr }
-	cmp		r0, #Upgrade_None
-	beq		@@return
-	cmp		r0, #Upgrade_IceBeam
-	blo		@@init_registers
-	mov		r0, #Upgrade_IceBeam
-@@init_registers:
-	mov		r6, r0
-	ldr		r5, =MajorUpgradeInfo
-	ldr		r4, =SamusUpgrades
-	mov		r3, #Upgrade_Missiles
-@@loop:
-	cmp		r3, r6
-	bgt		@@return
-	lsl		r0, r3, #2
-	add		r2, r5, r0
-	ldrb	r1, [r2, MajorUpgradeInfo_Bitmask]
-	ldrb	r2, [r2, MajorUpgradeInfo_Offset]
-	ldrb	r0, [r4, r2]
-	orr		r0, r1
-	strb	r0, [r4, r2]
-	add		r3, #1
-	b		@@loop
-@@return:
-	pop		{ r4-r6, pc }
+.org 080717BCh
+.area 110h
+	; init demo memory
+	ldr		r1, =CurrentDemo
+	ldrb	r1, [r1]
+	lsl		r1, #19h
+	lsr		r1, #19h
+	lsl		r2, r1, #3
+	sub		r2, r1
+	lsl		r1, r2, #2
+	ldr		r2, =DemoMemory
+	add		r2, r1
+	cmp		r0, #0
+	bne		@@second_step_init
+	ldr		r1, =CurrArea
+	ldrb	r0, [r2, DemoMemory_Area]
+	strb	r0, [r1]
+	mov		r0, #0
+	strb	r0, [r1, CurrRoom - CurrArea]
+	ldrb	r0, [r2, DemoMemory_PrevDoor]
+	strb	r0, [r1, PrevDoor - CurrArea]
+	ldrb	r0, [r2, DemoMemory_SecurityLevel]
+	ldr		r1, =SamusUpgrades
+	ldrb	r0, [r2, DemoMemory_SecurityLevel]
+	strb	r0, [r1, SamusUpgrades_SecurityLevel]
+	ldrb	r0, [r2, DemoMemory_MapDownloads]
+	strb	r0, [r1, SamusUpgrades_MapDownloads]
+	ldrb	r0, [r2, DemoMemory_BeamUpgrades]
+	strb	r0, [r1, SamusUpgrades_BeamUpgrades]
+	ldrb	r0, [r2, DemoMemory_ExplosiveUpgrades]
+	strb	r0, [r1, SamusUpgrades_ExplosiveUpgrades]
+	ldrb	r0, [r2, DemoMemory_SuitUpgrades]
+	strb	r0, [r1, SamusUpgrades_SuitUpgrades]
+	ldrh	r0, [r2, DemoMemory_MaxEnergy]
+	strh	r0, [r1, SamusUpgrades_MaxEnergy]
+	ldrh	r0, [r2, DemoMemory_CurrEnergy]
+	strh	r0, [r1, SamusUpgrades_CurrEnergy]
+	ldrh	r0, [r2, DemoMemory_MaxMissiles]
+	strh	r0, [r1, SamusUpgrades_MaxMissiles]
+	ldrh	r0, [r2, DemoMemory_CurrMissiles]
+	strh	r0, [r1, SamusUpgrades_CurrMissiles]
+	ldrb	r0, [r2, DemoMemory_MaxPowerBombs]
+	strb	r0, [r1, SamusUpgrades_MaxPowerBombs]
+	ldrb	r0, [r2, DemoMemory_CurrPowerBombs]
+	strb	r0, [r1, SamusUpgrades_CurrPowerBombs]
+	ldr		r1, =SamusState
+	ldrh	r0, [r2, DemoMemory_SamusDirection]
+	strh	r0, [r1, SamusState_Direction]
+	mov		r0, #0
+	strb	r0, [r1, SamusState_Pose]
+	add		r1, #20h
+	strb	r0, [r1, SamusState_AnimationFrame - 20h]
+	strb	r0, [r1, SamusState_Animation - 20h]
+	ldr		r0, =DemoTanksCollected
+	ldr		r1, =TanksCollected
+	ldr		r0, [r0]
+	str		r0, [r1]
+	bx		lr
+@@second_step_init:
+	ldr		r1, =SamusState
+	ldrh	r0, [r2, DemoMemory_SamusXPos]
+	strh	r0, [r1, SamusState_PositionX]
+	ldrh	r0, [r2, DemoMemory_SamusYPos]
+	strh	r0, [r1, SamusState_PositionY]
+	ldr		r1, =CurrentDemo
+	ldrb	r0, [r1]
+	lsl		r0, #19h
+	lsr		r0, #19h
+	strb	r0, [r1]
+	mov		r0, #0
+	ldr		r1, =ShortFrameCounter
+	strb	r0, [r1]
+	ldr		r1, =LongFrameCounter
+	strh	r0, [r1]
+	bx		lr
 	.pool
 .endarea
 
-.org 083E3F6Ch + 0 * 1Ch + 08h
-	.db		Upgrade_Bombs
+.org 0807DF04h
+.area 60h
+	bx		lr
+.endarea
 
-.org 083E3F6Ch + 1 * 1Ch + 08h
-	.db		Upgrade_IceMissiles
+.org DemoMemory + 0 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		1 << BeamUpgrade_ChargeBeam
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs)
+	.db		(1 << SuitUpgrade_MorphBall) 
+	.dh		0
 
-.org 083E3F6Ch + 2 * 1Ch + 08h
-	.db		Upgrade_HiJump
+.org DemoMemory + 1 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		1 << BeamUpgrade_ChargeBeam
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs) \
+		  | (1 << ExplosiveUpgrade_SuperMissiles) | (1 << ExplosiveUpgrade_IceMissiles)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump) \
+		  | (1 << SuitUpgrade_Speedbooster) | (1 << SuitUpgrade_VariaSuit)
+	.dh		0
 
-.org 083E3F6Ch + 3 * 1Ch + 08h
-	.db		Upgrade_Missiles
+.org DemoMemory + 2 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		1 << BeamUpgrade_ChargeBeam
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump)
+	.dh		0
 
-.org 083E3F6Ch + 4 * 1Ch + 08h
-	.db		Upgrade_MorphBall
+.org DemoMemory + 3 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		0
+	.db		1 << ExplosiveUpgrade_Missiles
+	.db		0
+	.dh		0
 
-.org 083E3F6Ch + 5 * 1Ch + 08h
-	.db		Upgrade_ScrewAttack
+.org DemoMemory + 4 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		0
+	.db		1 << ExplosiveUpgrade_Missiles
+	.db		1 << SuitUpgrade_MorphBall
+	.dh		0
 
-.org 083E3F6Ch + 6 * 1Ch + 08h
-	.db		Upgrade_Speedbooster
+.org DemoMemory + 5 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		(1 << BeamUpgrade_ChargeBeam) | (1 << BeamUpgrade_WideBeam) \
+		  | (1 << BeamUpgrade_PlasmaBeam) | (1 << BeamUpgrade_WaveBeam)
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs) \
+		  | (1 << ExplosiveUpgrade_SuperMissiles) | (1 << ExplosiveUpgrade_IceMissiles) \
+		  | (1 << ExplosiveUpgrade_PowerBombs) | (1 << ExplosiveUpgrade_DiffusionMissiles)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump) \
+		  | (1 << SuitUpgrade_Speedbooster) | (1 << SuitUpgrade_VariaSuit) \
+		  | (1 << SuitUpgrade_SpaceJump) | (1 << SuitUpgrade_GravitySuit) \
+		  | (1 << SuitUpgrade_ScrewAttack)
+	.dh		0
 
-.org 083E3F6Ch + 7 * 1Ch + 08h
-	.db		Upgrade_PowerBombs
+.org DemoMemory + 6 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		1 << BeamUpgrade_ChargeBeam
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump) \
+		  | (1 << SuitUpgrade_Speedbooster)
+	.dh		0
 
-.org 083E3F6Ch + 8 * 1Ch + 08h
-	.db		Upgrade_SuperMissiles
+.org DemoMemory + 7 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		(1 << BeamUpgrade_ChargeBeam) | (1 << BeamUpgrade_WideBeam)
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs) \
+		  | (1 << ExplosiveUpgrade_SuperMissiles) | (1 << ExplosiveUpgrade_IceMissiles) \
+		  | (1 << ExplosiveUpgrade_PowerBombs)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump) \
+		  | (1 << SuitUpgrade_Speedbooster) | (1 << SuitUpgrade_VariaSuit)
+	.dh		0
 
-.org 083E3F6Ch + 9 * 1Ch + 08h
-	.db		Upgrade_DiffusionMissiles
+.org DemoMemory + 8 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		1 << BeamUpgrade_ChargeBeam
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs) \
+		  | (1 << ExplosiveUpgrade_SuperMissiles)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump) \
+		  | (1 << SuitUpgrade_Speedbooster)
+	.dh		0
 
-.org 083E3F6Ch + 10 * 1Ch + 08h
-	.db		Upgrade_Missiles
+.org DemoMemory + 9 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		(1 << BeamUpgrade_ChargeBeam) | (1 << BeamUpgrade_WideBeam) \
+		  | (1 << BeamUpgrade_PlasmaBeam)
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs) \
+		  | (1 << ExplosiveUpgrade_SuperMissiles) | (1 << ExplosiveUpgrade_IceMissiles) \
+		  | (1 << ExplosiveUpgrade_PowerBombs) | (1 << ExplosiveUpgrade_DiffusionMissiles)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump) \
+		  | (1 << SuitUpgrade_Speedbooster) | (1 << SuitUpgrade_VariaSuit) \
+		  | (1 << SuitUpgrade_SpaceJump) | (1 << SuitUpgrade_GravitySuit)
+	.dh		0
 
-.org 083E3F6Ch + 11 * 1Ch + 08h
-	.db		Upgrade_ScrewAttack
+.org DemoMemory + 10 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		0
+	.db		1 << ExplosiveUpgrade_Missiles
+	.db		0
+	.dh		0
+
+.org DemoMemory + 11 * DemoMemory_Size + DemoMemory_BeamUpgrades
+	.db		(1 << BeamUpgrade_ChargeBeam) | (1 << BeamUpgrade_WideBeam) \
+		  | (1 << BeamUpgrade_PlasmaBeam) | (1 << BeamUpgrade_WaveBeam)
+	.db		(1 << ExplosiveUpgrade_Missiles) | (1 << ExplosiveUpgrade_Bombs) \
+		  | (1 << ExplosiveUpgrade_SuperMissiles) | (1 << ExplosiveUpgrade_IceMissiles) \
+		  | (1 << ExplosiveUpgrade_PowerBombs) | (1 << ExplosiveUpgrade_DiffusionMissiles)
+	.db		(1 << SuitUpgrade_MorphBall) | (1 << SuitUpgrade_HiJump) \
+		  | (1 << SuitUpgrade_Speedbooster) | (1 << SuitUpgrade_VariaSuit) \
+		  | (1 << SuitUpgrade_SpaceJump) | (1 << SuitUpgrade_GravitySuit) \
+		  | (1 << SuitUpgrade_ScrewAttack)
+	.dh		0
