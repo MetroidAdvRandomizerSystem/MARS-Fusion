5f67c0938ee2c0a88cbea0decf6b938f571a75c6
Callie LeFave <antymew@gmail.com>
2024-03-04 21:21:20 -0500

    Change main deck and tourian music

--- src/nonlinear/music.s
+++ src/nonlinear/music.s
@@ -112,7 +112,7 @@
 @@case_MainDeck_check26:
 	; arachnus fight room
 	cmp		r6, #26h
-	bne		@@case_MainDeck_check4D
+	bne		@@case_MainDeck_check36
 	ldr		r0, [r2, MiscProgress_MajorLocations]
 	lsr		r0, MajorLocation_Arachnus + 1
 	bcs		@@case_MainDeck_default
@@ -124,6 +124,16 @@
 	mov		r1, MusicType_BossAmbience
 	mov		r2, #60
 	b		@@tryLock
+@@case_MainDeck_check36:
+	cmp		r6, #36h
+	bne		@@case_MainDeck_check4D
+	ldr		r0, [r2, MiscProgress_StoryFlags]
+	lsr		r0, StoryFlag_AuxiliaryPower + 1
+	bcs		@@case_MainDeck_default
+	mov		r0, #0Fh
+	mov		r1, #MusicType_Transient
+	mov		r2, #10
+	b		@@tryPlay
 @@case_MainDeck_check4D:
 .if !RANDOMIZER
 	; restricted sector tube
@@ -540,3 +550,122 @@
 ; Remove subevent 0 music handling from dialogue handling
 .org 08079ED4h
 	nop :: nop :: nop :: nop
+
+; change default music in certain rooms
+
+.org 083C2C4Ch + 07h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck station entrance
+.org 083C2C4Ch + 08h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck recharge room
+.org 083C2C4Ch + 09h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck navigation room east
+.org 083C2C4Ch + 0Ah * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck crew quarters east
+.org 083C2C4Ch + 0Bh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck habitation save room
+.org 083C2C4Ch + 0Ch * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck crew quarters west
+.org 083C2C4Ch + 0Eh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck maintenance corridor
+.org 083C2C4Ch + 0Fh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck habitation deck entrance
+.org 083C2C4Ch + 10h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck navigation room west
+.org 083C2C4Ch + 12h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck central hub
+.org 083C2C4Ch + 14h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck western hub
+.org 083C2C4Ch + 15h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck eastern hub
+.org 083C2C4Ch + 16h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck habitation maintenance
+.org 083C2C4Ch + 17h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck quarantine access
+.org 083C2C4Ch + 21h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck save room
+.org 083C2C4Ch + 23h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck maintenance shaft
+.org 083C2C4Ch + 24h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck maintenance crossing
+.org 083C2C4Ch + 25h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck crew quarters save room
+.org 083C2C4Ch + 26h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck arachnus arena
+.org 083C2C4Ch + 28h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck main elevator
+.org 083C2C4Ch + 2Ah * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck main elevator access
+.org 083C2C4Ch + 2Dh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck maintenance storage
+.org 083C2C4Ch + 2Eh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck sub-zero containment
+.org 083C2C4Ch + 2Fh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck spitter speedway
+.org 083C2C4Ch + 39h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck habitation storage
+.org 083C2C4Ch + 3Ch * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck elevator to crew quarters
+.org 083C2C4Ch + 3Dh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck elevator to operations deck
+.org 083C2C4Ch + 45h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck habitation deck
+.org 083C2C4Ch + 46h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck hornoad hallway
+.org 083C2C4Ch + 47h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck quarantine bay
+.org 083C2C4Ch + 48h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck western hub cache
+.org 083C2C4Ch + 4Bh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck elevator to habitation deck
+.org 083C2C4Ch + 4Ch * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck elevator to central hub
+.org 083C2C4Ch + 54h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Ch		; main deck the attic
+
+.org 083C2C4Ch + 0Dh * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck operations deck
+.org 083C2C4Ch + 20h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck operations deck navigation room
+.org 083C2C4Ch + 27h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck operations deck data room
+.org 083C2C4Ch + 2Ch * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck operations deck save room
+.org 083C2C4Ch + 36h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck auxiliary power station
+.org 083C2C4Ch + 51h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck operations deck recharge room
+.org 083C2C4Ch + 51h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck operations room
+.org 083C2C4Ch + 55h * LevelMeta_Size + LevelMeta_Music
+	.dh		2Eh		; main deck sa-x arena
+
+.org 083C40B0h + 12h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 tourian shaft east
+.org 083C40B0h + 13h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 tourian save room east
+.org 083C40B0h + 15h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 tourian central hub
+.org 083C40B0h + 16h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 golden pirate crossing
+.org 083C40B0h + 17h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 tourian hub west
+.org 083C40B0h + 18h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 tourian save room west
+.org 083C40B0h + 19h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 genesis habitation
+.org 083C40B0h + 1Ah * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 ridley arena access
+.org 083C40B0h + 1Bh * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 ridley arena
+.org 083C40B0h + 1Ch * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 tourian central checkpoint
+.org 083C40B0h + 1Dh * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 ripper maze access
+.org 083C40B0h + 1Eh * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 ripper maze
+.org 083C40B0h + 24h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 tourian entrance
+.org 083C40B0h + 33h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 animorphs
+.org 083C40B0h + 34h * LevelMeta_Size + LevelMeta_Music
+	.dh		35h		; sector 1 animorphs cache
