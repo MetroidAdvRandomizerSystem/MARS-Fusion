7158526897f01548429c85774d3481b2e62fefe5
Callie LeFave <antymew@gmail.com>
2024-03-09 01:52:42 -0500

    Optimize power bomb explosion collision check

---
+++ src/optimization/power-bomb-explosion.s
@@ -0,0 +1,18 @@
+; Converts floating-point arithmetic to fixed-point reciprocal multiplication
+; for calculating the size of a power bomb explosion. Saves an average of
+; 10050 cycles per frame.
+
+.org 08068126h
+.area 34h
+    ; Multiply by 19/20 using fixed-point reciprocal multiplication
+    ldrb    r0, [r7, #2]
+    lsl     r0, #3
+    ldr     r1, =#19 * (((1 << 20) - 1) / 20 + 1)
+    mul     r0, r1
+    lsr     r0, #20
+    mov     r6, r0
+    lsr     r0, #1
+    mov     r8, r0
+    b       0806815Ah
+    .pool
+.endarea
