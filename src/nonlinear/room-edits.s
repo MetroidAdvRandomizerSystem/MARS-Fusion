3762eb23e86e045fdfbed2f9ff95a76540f37d53
Callie LeFave <antymew@gmail.com>
2024-03-14 18:54:46 -0400

    Add spriteset changes to room edits assembly

--- src/nonlinear/room-edits.s
+++ src/nonlinear/room-edits.s
@@ -1,5 +1,25 @@
 ; Room edits for open exploration, enemies, softlock prevention, etc.
 
+; Debug room data
+.defineregion 083C2A48h, 3 * 90h
+.defineregion 083C2C10h, 3Ch
+.defineregion 083F1548h, 0C56h
+
+; Main Deck - Crew Quarters West
+; remove power bomb geron
+.org MainDeckLevels + 0Ch * LevelMeta_Size + LevelMeta_Spriteset1Event
+.area LevelMeta_MapX - LevelMeta_Spriteset1Event
+    .db     63h
+    .skip 2
+    .dw     08470F0Ch
+    .db     33h
+    .db     0
+    .skip 2
+    .dw     083BF884h
+    .db     0
+.endarea
+.defineregion 08470FB2h, 0Fh
+
 ; Main Deck - Operations Deck
 ; change operations room lv4 security door to lv0 security
 .org 08471668h
@@ -11,8 +31,51 @@
 .incbin "data/rooms/S0-0D-Clip.rlebg"
 .endarea
 
+; Main Deck - Central Hub
+; keep power bomb geron always loaded
+.org MainDeckLevels + 12h * LevelMeta_Size + LevelMeta_Spriteset0
+.area LevelMeta_Spriteset2Event - LevelMeta_Spriteset0
+    .dw     0847283Bh
+    .db     33h
+    .db     0
+    .skip 2
+    .dw     083BF884h
+    .db     0
+.endarea
+.defineregion 08472AB0h, 03h
+
+; Main Deck - Eastern Hub
+; remove geron in front of recharge station
+.org MainDeckLevels + 15h * LevelMeta_Size + LevelMeta_Spriteset0
+.area LevelMeta_MapX - LevelMeta_Spriteset0
+    .dw     0847344Fh
+    .db     19h
+    .db     63h
+    .skip 2
+    .dw     0847334Ch
+    .db     19h
+    .db     0
+    .skip 2
+    .dw     083BF884h
+    .db     0
+.endarea
+.defineregion 084736C9h, 12h
+
+; Main Deck - Sector Hub
+; keep main elevator always active
+.org MainDeckLevels + 18h * LevelMeta_Size + LevelMeta_Spriteset0
+.area LevelMeta_Spriteset2Event - LevelMeta_Spriteset0
+    .dw     08473F3Ah
+    .db     02h
+    .db     0
+    .skip 2
+    .dw     083BF884h
+    .db     0
+.endarea
+.defineregion 08474195h, 27h
+
 ; Main Deck - Maintenance Shaft
-; repair maintenance crossing
+; repair maintenance crossing and add a geron
 .org 08476337h
 .area 492h
 .incbin "data/rooms/S0-23-BG1.rlebg"
@@ -22,8 +85,27 @@
 .incbin "data/rooms/S0-23-BG2.rlebg"
 .endarea
 
+.autoregion
+@MaintenanceShaft_Spriteset0:
+    .db     05h, 0Eh, 25h
+    .db     15h, 0Bh, 03h
+    .db     19h, 0Ch, 12h
+    .db     20h, 07h, 38h
+    .db     46h, 0Bh, 22h
+    .db     39h, 0Ch, 24h
+    .db     0FFh, 0FFh, 0FFh
+.endautoregion
+
+.org MainDeckLevels + 23h * LevelMeta_Size + LevelMeta_Spriteset0
+.area LevelMeta_Spriteset1Event - LevelMeta_Spriteset0
+    .dw     @MaintenanceShaft_Spriteset0
+    .db     19h
+.endarea
+
+.defineregion 084767C9h, 12h
+
 ; Main Deck - Maintenance Crossing
-; repair so the crossing is traversable
+; repair so the crossing is traversable and add a geron
 .org 0847690Fh
 .area 100h
 .incbin "data/rooms/S0-24-BG1.rlebg"
@@ -37,6 +119,32 @@
 .incbin "data/rooms/S0-24-Clip.rlebg"
 .endarea
 
+.autoregion
+@MaintenanceCrossing_Spriteset0:
+    .db     07h, 0Ch, 24h
+    .db     0FFh, 0FFh, 0FFh
+.endautoregion
+
+.org MainDeckLevels + 24h * LevelMeta_Size + LevelMeta_Spriteset0
+.area LevelMeta_Spriteset1Event - LevelMeta_Spriteset0
+    .dw     @MaintenanceCrossing_Spriteset0
+    .db     19h
+.endarea
+
+.defineregion 08476A0Fh, 03h
+
+; Main Deck - Silo Access
+; move zoro cocoon
+.org 08479091h
+.area 03h
+    .db     15h, 05h, 14h
+.endarea
+
+.org 08478E46h
+.area 03h
+    .db     15h, 05h, 14h
+.endarea
+
 ; Main Deck - Operations Room
 ; change lv4 security door to lv0 security
 .org 084814F8h
@@ -48,8 +156,25 @@
 .incbin "data/rooms/S0-52-Clip.rlebg"
 .endarea
 
+; Sector 2 - Data Hub Access
+; move cocoon and kihunter spritesets to intact room state
+.org Sector2Levels + 03h * LevelMeta_Size + LevelMeta_Spriteset1Event
+.area LevelMeta_MapX - LevelMeta_Spriteset1Event
+    .db     19h
+    .skip 2
+    .dw     084D5246h
+    .db     13h
+    .db     47h
+    .skip 2
+    .dw     084D50FCh
+    .db     1Eh
+.endarea
+
 ; Sector 2 - Central Shaft
-;
+; make door to reo room functional
+; remove hatch to ripper roost
+; move zoro out of the way of ripper roost
+; move cocoon and kihunter spritesets to intact room state
 .org 084D0D55h
 .area 4E1h
 .incbin "data/rooms/S2-0D-BG1.rlebg"
@@ -58,6 +183,51 @@
 .area 1F0h
 .incbin "data/rooms/S2-0D-Clip.rlebg"
 .endarea
+
+.org 084D1236h + 1 * 03h
+.area 03h
+    .db     1Bh, 05h, 24h
+.endarea
+
+.org 084D9BDFh + 1 * 03h
+.area 03h
+    .db     1Bh, 05h, 14h
+.endarea
+
+.org 084D96BEh + 2 * 03h
+.area 03h
+    .db     1Bh, 05h, 14h
+.endarea
+
+.org Sector2Levels + 0Dh * LevelMeta_Size + LevelMeta_Spriteset1Event
+.area LevelMeta_MapX - LevelMeta_Spriteset1Event
+    .db     19h
+    .skip 2
+    .dw     084D9BDFh
+    .db     13h
+    .db     47h
+    .skip 2
+    .dw     084D96BEh
+    .db     1Eh
+.endarea
+
+; Sector 2 - Dessgeega Dormitory
+; add cocoon and kihunter spritesets to intact room state
+.org Sector2Levels + 0Eh * LevelMeta_Size + LevelMeta_Spriteset1Event
+.area LevelMeta_MapX - LevelMeta_Spriteset1Event
+    .db     19h
+    .skip 2
+    .dw     084D8F6Fh
+    .db     13h
+    .db     47h
+    .skip 2
+    .dw     084D8E00h
+    .db     1Eh
+.endarea
+
+; Sector 2 - Zazabi Access
+; add cocoon and kihunter spritesets to intact room state
+; TODO
 
 ; Sector 2 - Data Hub
 ; repair door to data hub access
@@ -118,6 +288,22 @@
 .incbin "data/rooms/S4-22-Clip.rlebg"
 .endarea
 
+; Sector 4 - Drain Pipe
+; keep puffer always active
+.org Sector4Levels + 24h * LevelMeta_Size + LevelMeta_Spriteset1Event
+.area LevelMeta_MapX - LevelMeta_Spriteset1Event
+    .db     20h
+    .skip 2
+    .dw     0854032Eh
+    .db     23h
+    .db     0
+    .skip 2
+    .dw     083BF884h
+    .db     0
+.endarea
+
+.defineregion 085403DBh, 2Ah
+
 ; Sector 5 - Nightmare Training Grounds
 ; restructure the room to have a speedbooster runway across the top
 ; add speedbooster blocks above the power bomb blocks
@@ -142,6 +328,7 @@
 .endarea
 
 ; Sector 5 - Data Room
+; merge the intact and destroyed data rooms into a single room
 ; seal off the destroyed upper half of the data room from the intact lower half
 .org 08519419h
 .area 19Fh
@@ -150,6 +337,16 @@
 .org 085191A4h
 .area 84h
 .incbin "data/rooms/S5-10-Clip.rlebg"
+.endarea
+
+.org 085195B8h
+.area 03h
+    .db     14h, 09h, 11h
+.endarea
+
+.org Sector5Levels + 10h * LevelMeta_Size + LevelMeta_Spriteset0Id
+.area 01h
+    .db     0Ch
 .endarea
 
 ; Sector 5 - Security Shaft East
