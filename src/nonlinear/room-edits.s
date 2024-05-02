71007bb1508f4237f4871197c9e24398cbea868f
Callie LeFave <antymew@gmail.com>
2024-05-02 12:10:54 -0400

    Fix S3 Security Access and S5 Groznyj Grad room edits

--- src/nonlinear/room-edits.s
+++ src/nonlinear/room-edits.s
@@ -484,6 +484,20 @@
 .incbin "data/rooms/S2-36-Clip.rlebg"
 .endarea
 
+; Sector 3 - Security Access
+; remove sidehoppers on speedbooster runway to prevent near softlock with neither charge nor missiles
+; move sidehoppers below runway to prevent them from clipping into the wall
+.org readptr(Sector3Levels + 03h * LevelMeta_Size + LevelMeta_Spriteset1)
+.area 27h
+    .db     04h, 24h, 0A4h
+    .db     05h, 1Fh, 0A4h
+    .db     05h, 29h, 0A4h
+    .db     06h, 1Dh, 02h
+    .db     14h, 29h, 23h
+    .db     14h, 2Ch, 23h
+    .db     0FFh, 0FFh, 0FFh
+.endarea
+
 ; Sector 3 - Bob's Room
 ; remove event-based transitions leading to wrecked room state
 .org Sector3Doors + 0Ch * DoorEntry_Size + DoorEntry_Type
@@ -903,7 +917,7 @@
 
 ; Sector 5 - Groznyj Grad
 ; fix screen scrolls when entering from Security Shaft East
-.defineregion readptr(Sector4Scrolls + 0Fh * 4), ScrollList_HeaderSize + Scroll_Size * 1
+.defineregion readptr(Sector5Scrolls + 0Fh * 4), ScrollList_HeaderSize + Scroll_Size * 1
 
 ; Sector 5 scroll table fixes
 .org Sector5Scrolls
