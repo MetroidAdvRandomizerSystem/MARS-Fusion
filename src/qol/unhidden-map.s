ec742e0ef9d24bbf104158b263dd07eb1c9382e7
clue cat <gitlab@clue.cat>
2024-09-07 23:44:08 -0400

    adjust minimaps to reflect navigable rooms fixes #68

--- src/qol/unhidden-map.s
+++ src/qol/unhidden-map.s
@@ -13,3 +13,19 @@
     .dh     2A25h, 7FFFh, 39CEh, 39CEh, 4800h, 2484h, 7FFFh, 03FFh
     .dh     7C00h, 43E2h, 249Fh, 03FFh, 7C00h, 43E2h, 249Fh, 03FFh
 .endarea
+
+; map adjustments to match room adjustments
+.org readptr(MinimapDataPointers + MainDeckMinimap)
+.area 24Bh
+.incbin "data/maps/maindeck.lz77"
+.endarea
+
+.org readptr(MiniMapDataPointers + Sector3Minimap)
+.area 1E3h
+.incbin "data/maps/sector3.lz77"
+.endarea
+
+.org readptr(MiniMapDataPointers + Sector5Minimap)
+.area 1F5h
+.incbin "data/maps/sector5.lz77"
+.endarea
