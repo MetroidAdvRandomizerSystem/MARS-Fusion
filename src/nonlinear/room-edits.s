06d529b29ab04b09f4c277718a42935f18a7fc78
clue cat <gitlab@clue.cat>
2024-09-23 23:57:02 -0400

    sector 2: Shooting Gallery Edits. Edits the bottom door connection to point to the before-nettori defeated room state by default and adds an event-based door connection.

--- src/nonlinear/room-edits.s
+++ src/nonlinear/room-edits.s
@@ -687,6 +687,24 @@
 .endarea
 .endif
 
+; Sector 2 - Shooting Gallery
+; change lower door connection to be event based
+.org VariableConnections + (0Bh * VariableConnection_Size)
+.area 4
+    .db Area_TRO, 19h
+    .db Event_NettoriAbsorbed, 4Bh
+.endarea
+
+.org Sector2Doors + 19h * DoorEntry_Size + DoorEntry_Type
+.area 1
+    .db 24h
+.endarea
+
+.org Sector2Doors + 19h * DoorEntry_Size + DoorEntry_Destination
+.area 1
+    .db 2Fh
+.endarea
+
 ; Sector 3 - Security Access
 ; remove sidehoppers on speedbooster runway to prevent near softlock with neither charge nor missiles
 ; move sidehoppers below runway to prevent them from clipping into the wall
