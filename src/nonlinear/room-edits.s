573e090723c3089c2c150d8638c8b2fd76d85703
clue cat <gitlab@clue.cat>
2024-10-06 15:41:39 -0400

    sector2: adjust clipdata in Nettori Access. Removes 4 tiles to prevent grounded ki-hunter from jumping through solid walls

--- src/nonlinear/room-edits.s
+++ src/nonlinear/room-edits.s
@@ -586,6 +586,17 @@
 
 ; Sector 2 - Nettori Access
 ; change winged kihunter below eyedoor into a grounded kihunter
+; remove 4 tiles to prevent the above kihunter from jumping through solids
+.org readptr(Sector2Levels + 14h * LevelMeta_Size + LevelMeta_Bg1)
+.area 4F6h
+.incbin "data/rooms/S2-14-BG1.rlebg"
+.endarea
+
+.org readptr(Sector2Levels + 14h * LevelMeta_Size + LevelMeta_Clipdata)
+.area 1AEh
+.incbin "data/rooms/S2-14-Clip.rlebg"
+.endarea
+
 .org readptr(Sector2Levels + 14h * LevelMeta_Size + LevelMeta_Spriteset0) + 3 * 3
 .area 3
     .db     11h, 15h, 26h
