fe118230c721481385f2d2b9bcd77793f8093a6b
Callie LeFave <antymew@gmail.com>
2024-10-12 09:06:42 -0400

    Fix S4 Aquarium Storage regression (resolves #100)

--- src/nonlinear/room-edits.s
+++ src/nonlinear/room-edits.s
@@ -941,10 +941,12 @@
 
 ; Sector 4 - Aquarium Storage
 ; make kago always block missile tank
-.defineregion readptr(Sector4Levels + 26h * LevelMeta_Size + LevelMeta_Spriteset1), 24h
+.defineregion readptr(Sector4Levels + 26h * LevelMeta_Size + LevelMeta_Spriteset0), 24h
 
-.org Sector4Levels + 26h * LevelMeta_Size + LevelMeta_Spriteset1Event
-.area LevelMeta_Spriteset2Event - LevelMeta_Spriteset1Event
+.org Sector4Levels + 26h * LevelMeta_Size + LevelMeta_Spriteset0
+.area LevelMeta_Spriteset2Event - LevelMeta_Spriteset0
+    .dw     readptr(Sector4Levels + 26h * LevelMeta_Size + LevelMeta_Spriteset1)
+    .skip 1
     .db     0
     .skip 2
     .dw     NullSpriteset
