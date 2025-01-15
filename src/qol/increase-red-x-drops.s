f6f694b283d793050dfa9059a742de0289d6b2d4
zarakava <zarakava@zarakorner.com>
2025-01-15 12:33:22 -0600

    Increase Drop Chance to 12/1024 - add additional enemies that need increase

--- src/qol/increase-red-x-drops.s
+++ src/qol/increase-red-x-drops.s
@@ -9,182 +9,218 @@
 
 .org SpriteStats + Hornoad_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Halzyn_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 2D3/12C/1
-    .dh 02D0h
+    .dh 02C8h
     .dh 012Ch
-    .dh 0004h
+    .dh 000Ch
+
+.org SpriteStats + ZebesianWall_Id * SpriteStats_Size + SpriteStats_YellowXWeight
+    ; originally 2CA/12C/A
+    .dh 02C8h
+    .dh 012Ch
+    .dh 000Ch
 
 .org SpriteStats + Moto_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 2D3/12C/1
-    .dh 02D0h
+    .dh 02C8h
     .dh 012Ch
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Yameba_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
     
 .org SpriteStats + Zeela_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Zombie_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 20B/1F4/1
-    .dh 0208h
+    .dh 0200h
     .dh 01F4h
-    .dh 0004h
+    .dh 000Ch
     
 .org SpriteStats + Geemer_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 3F4/A/2
-    .dh 03F2h
+    .dh 03EAh
     .dh 000Ah
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Waver_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 305/FA/1
-    .dh 0302h;
+    .dh 02FAh;
     .dh 00FAh;
-    .dh 0004h;
+    .dh 000Ch;
 
 .org SpriteStats + Sciser_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Sidehopper_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 20B/1F4/1
-    .dh 0208h
+    .dh 0200h
     .dh 01F4h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Dessgeega_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 322/DC/2
-    .dh 0320h
+    .dh 0318h
     .dh 00DCh
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Zoro_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 305/FA/1
-    .dh 0302h;
+    .dh 02FAh;
     .dh 00FAh;
-    .dh 0004h;
+    .dh 000Ch;
 
 
 .org SpriteStats + KihunterFlying_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + KihunterGround_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Reo_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 369/96/1
-    .dh 0366h
+    .dh 035Eh
     .dh 0096h
-    .dh 0004h
+    .dh 000Ch
     
 .org SpriteStats + Namihe_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 2D1/12C/3
-    .dh 02D0h
+    .dh 02C8h
     .dh 012Ch
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Fune_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 2D1/12C/3
-    .dh 02D0h
+    .dh 02C8h
     .dh 012Ch
-    .dh 0004h
+    .dh 000Ch
 
 
 .org SpriteStats + Geruda_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + SkulteraSmallNotLarge_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Sova_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 39B/64/1
-    .dh 0398h
+    .dh 0390h
     .dh 0064h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Yard_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 26E/190/2
-    .dh 026Ch
+    .dh 0264h
     .dh 0190h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Evir_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 209/1F4/3
-    .dh 0208h
+    .dh 0200h
     .dh 01F4h
-    .dh 0004h;
+    .dh 000Ch;
     
 .org SpriteStats + Bull_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Memu_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 39B/64/1
-    .dh 0398h
+    .dh 0390h
     .dh 0064h
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Choot_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
+
+.org SpriteStats + ZebesianGround_Id * SpriteStats_Size + SpriteStats_YellowXWeight
+    ; originally 2CA/12C/A
+    .dh 02C8h
+    .dh 012Ch
+    .dh 000Ch
 
 .org SpriteStats + GoldSciser_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 175/28A/1
-    .dh 0172h
+    .dh 016Ah
     .dh 028Ah
-    .dh 0004h
+    .dh 000Ch
 
 .org SpriteStats + Owtch_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 369/96/1
-    .dh 0366h
+    .dh 035Eh
     .dh 0096h
-    .dh 0004h
+    .dh 000Ch
+
+.org SpriteStats + Genesis_Id * SpriteStats_Size + SpriteStats_YellowXWeight
+    ; originally 2CA/12C/A
+    .dh 02C8h
+    .dh 012Ch
+    .dh 000Ch    
 
 .org SpriteStats + Puyo_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 39B/64/1
-    .dh 0398h
+    .dh 0390h
     .dh 0064h
-    .dh 0004h
+    .dh 000Ch
+
+.org SpriteStats + ZebesianAqua_Id * SpriteStats_Size + SpriteStats_YellowXWeight
+    ; originally 2CA/12C/A
+    .dh 02C8h
+    .dh 012Ch
+    .dh 000Ch
+
+.org SpriteStats + ZebesianPreAqua_Id * SpriteStats_Size + SpriteStats_YellowXWeight
+    ; originally 2CA/12C/A
+    .dh 02C8h
+    .dh 012Ch
+    .dh 000Ch
 
 .org SpriteStats + SkulteraSmall_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 337/C8/1
-    .dh 0334h
+    .dh 032Ch
     .dh 00C8h
-    .dh 0004h
+    .dh 000Ch
+
+.org SpriteStats + Powamp_Id * SpriteStats_Size + SpriteStats_YellowXWeight
+    ; originally 32E/C8/A
+    .dh 032Ch
+    .dh 00C8h
+    .dh 000Ch
 
 .org SpriteStats + Zozoro_Id * SpriteStats_Size + SpriteStats_YellowXWeight
     ; originally 2D3/12C/1
-    .dh 02D0h
+    .dh 02C8h
     .dh 012Ch
-    .dh 0004h
+    .dh 000Ch
