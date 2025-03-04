aeaa851379966e8075b69ae8f879743dd84c2a51
biosp4rk <37962487+biosp4rk@users.noreply.github.com>
2025-03-03 16:18:49 -0800

    Lower ice beam volume
    Lowers ice beam volume by 40%

---
+++ src/qol/ice-beam-volume.s
@@ -0,0 +1,125 @@
+; Fire uncharged (CD) track 0
+.org 0827D357h
+    .db round(41h * IceVolumeFactor)
+.org 0827D363h
+    .db round(39h * IceVolumeFactor)
+.org 0827D36Dh
+    .db round(3Dh * IceVolumeFactor)
+.org 0827D372h
+    .db round(41h * IceVolumeFactor)
+.org 0827D382h
+    .db round(41h * IceVolumeFactor)
+.org 0827D387h
+    .db round(2Dh * IceVolumeFactor)
+.org 0827D38Ch
+    .db round(21h * IceVolumeFactor)
+.org 0827D391h
+    .db round(18h * IceVolumeFactor)
+.org 0827D396h
+    .db round(11h * IceVolumeFactor)
+.org 0827D39Bh
+    .db round(0Ch * IceVolumeFactor)
+.org 0827D3A0h
+    .db round(07h * IceVolumeFactor)
+.org 0827D3A5h
+    .db round(03h * IceVolumeFactor)
+
+; Fire uncharged (CD) track 1
+.org 0827D3AFh
+    .db round(38h * IceVolumeFactor)
+.org 0827D3BCh
+    .db round(35h * IceVolumeFactor)
+.org 0827D3C1h
+    .db round(32h * IceVolumeFactor)
+.org 0827D3C6h
+    .db round(2Fh * IceVolumeFactor)
+.org 0827D3CBh
+    .db round(2Ch * IceVolumeFactor)
+.org 0827D3D0h
+    .db round(28h * IceVolumeFactor)
+.org 0827D3D5h
+    .db round(25h * IceVolumeFactor)
+.org 0827D3DAh
+    .db round(22h * IceVolumeFactor)
+.org 0827D3DFh
+    .db round(1Fh * IceVolumeFactor)
+.org 0827D3E4h
+    .db round(1Ch * IceVolumeFactor)
+.org 0827D3E9h
+    .db round(19h * IceVolumeFactor)
+.org 0827D3EEh
+    .db round(16h * IceVolumeFactor)
+.org 0827D3F3h
+    .db round(13h * IceVolumeFactor)
+.org 0827D3F8h
+    .db round(10h * IceVolumeFactor)
+.org 0827D3FDh
+    .db round(0Dh * IceVolumeFactor)
+.org 0827D402h
+    .db round(0Ah * IceVolumeFactor)
+.org 0827D407h
+    .db round(07h * IceVolumeFactor)
+.org 0827D40Ch
+    .db round(04h * IceVolumeFactor)
+.org 0827D411h
+    .db round(01h * IceVolumeFactor)
+
+; Fire charged (F1) track 0
+.org 0827E287h
+    .db round(5Ah * IceVolumeFactor)
+.org 0827E295h
+    .db round(50h * IceVolumeFactor)
+.org 0827E29Dh
+    .db round(55h * IceVolumeFactor)
+
+; Fire charged (F1) track 1
+.org 0827E2D3h
+    .db round(4Dh * IceVolumeFactor)
+.org 0827E2E0h
+    .db round(4Dh * IceVolumeFactor)
+.org 0827E2E7h
+    .db round(4Ah * IceVolumeFactor)
+.org 0827E2EEh
+    .db round(46h * IceVolumeFactor)
+.org 0827E2F3h
+    .db round(43h * IceVolumeFactor)
+.org 0827E2F8h
+    .db round(3Fh * IceVolumeFactor)
+.org 0827E2FDh
+    .db round(3Ah * IceVolumeFactor)
+.org 0827E303h
+    .db round(32h * IceVolumeFactor)
+.org 0827E30Ah
+    .db round(1Eh * IceVolumeFactor)
+.org 0827E30Fh
+    .db round(17h * IceVolumeFactor)
+.org 0827E314h
+    .db round(11h * IceVolumeFactor)
+.org 0827E319h
+    .db round(0Dh * IceVolumeFactor)
+.org 0827E31Eh
+    .db round(0Ah * IceVolumeFactor)
+.org 0827E323h
+    .db round(07h * IceVolumeFactor)
+.org 0827E328h
+    .db round(05h * IceVolumeFactor)
+.org 0827E32Dh
+    .db round(03h * IceVolumeFactor)
+.org 0827E332h
+    .db round(02h * IceVolumeFactor)
+
+; Charging (E9) track 0
+.org 0827DF17h
+    .db round(37h * IceVolumeFactor)
+
+; Charging (E9) track 1
+.org 0827DF8Dh
+    .db round(2Ch * IceVolumeFactor)
+
+; Charged (EA) track 0
+.org 0827DFBFh
+    .db round(37h * IceVolumeFactor)
+
+; Charged (EA) track 1
+.org 0827DFD4h
+    .db round(2Ch * IceVolumeFactor)
