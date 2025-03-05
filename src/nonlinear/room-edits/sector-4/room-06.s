c9bf3dbe54348db5d36c78ecd57386af17825ad1
Miepee <janbidler00@protonmail.com>
2025-03-05 14:24:32 +0100

    Fix scizer not being at the wall in S4-06

---
+++ src/nonlinear/room-edits/sector-4/room-06.s
@@ -0,0 +1,18 @@
+; Reservoir East
+
+; Moves Scizer on Post-Water Pump to be back at the wall instead of at the floor
+.org readptr(Sector4Levels + 06h * LevelMeta_Size + LevelMeta_Spriteset1) + (3 * Spriteset_SpriteSize)
+.area 3
+    .db 0Dh, 26h, 24h
+.endarea
+
+
+; Replace shot-blocks to power bomb tank with never-reform variant
+.if ANTI_SOFTLOCK
+
+.org readptr(Sector4Levels + 06h * LevelMeta_Size + LevelMeta_Clipdata)
+.area 1D1h
+.incbin "data/rooms/S4-06-Clip.rlebg"
+.endarea
+
+.endif
